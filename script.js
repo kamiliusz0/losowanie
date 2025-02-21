e274624cfa864ada70114e84469c4d331bd7adce
kamiliusz0 <kamilwojtachaa@gmail.com>
2025-02-21 18:38:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,210 +1,94 @@
-const _0x2f917c = _0x2649;
-(function (_0x291dff, _0x5b73d6) {
-    const _0x191924 = _0x2649, _0x8e25f3 = _0x291dff();
-    while (!![]) {
-        try {
-            const _0x949461 = parseInt(_0x191924(0x1c8)) / (0x1733 + 0xa79 + -0x21ab) + parseInt(_0x191924(0x1b8)) / (-0x11 * 0x188 + 0x8a7 + 0x1163) * (-parseInt(_0x191924(0x198)) / (-0x24a5 * -0x1 + 0x1a26 * 0x1 + -0x62 * 0xa4)) + parseInt(_0x191924(0x1d2)) / (0x788 + 0x1 * -0x169f + 0xf1b) + parseInt(_0x191924(0x1c6)) / (0x3 * -0x875 + 0x21cc + 0x10d * -0x8) * (parseInt(_0x191924(0x1ab)) / (-0x4 * -0x39c + 0x77d + 0x10b * -0x15)) + parseInt(_0x191924(0x1de)) / (0x9 * 0x36 + 0xb67 * -0x2 + 0x14ef) * (parseInt(_0x191924(0x1d9)) / (0xd09 + -0x3 * -0x337 + -0x16a6)) + -parseInt(_0x191924(0x1c1)) / (-0x41c * -0x7 + -0x1 * -0x19b5 + -0x3670) + parseInt(_0x191924(0x19f)) / (-0xab * -0x2 + 0x1 * 0x1c0a + -0x1d56);
-            if (_0x949461 === _0x5b73d6)
-                break;
-            else
-                _0x8e25f3['push'](_0x8e25f3['shift']());
-        } catch (_0x56f048) {
-            _0x8e25f3['push'](_0x8e25f3['shift']());
+let names = [];
+
+const nameInput = document.getElementById('nameInput');
+const addButton = document.getElementById('addButton');
+const drawButton = document.getElementById('drawButton');
+const nameList = document.getElementById('nameList');
+const result = document.getElementById('result');
+
+// Funkcja do wyświetlania listy osób
+function renderNameList() {
+    nameList.innerHTML = ''; // Czyści listę przed ponownym wyświetleniem
+    names.forEach((name, index) => {
+        const nameItem = document.createElement('div');
+        nameItem.className = 'name-item';
+
+        const nameText = document.createElement('span');
+        nameText.textContent = name;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Usuń';
+        deleteButton.addEventListener('click', () => {
+            names.splice(index, 1); // Usuwa osobę z listy
+            renderNameList(); // Ponownie renderuje listę
+        });
+
+        nameItem.appendChild(nameText);
+        nameItem.appendChild(deleteButton);
+        nameList.appendChild(nameItem);
+    });
+}
+
+// Dodawanie imienia do listy
+addButton.addEventListener('click', () => {
+    const name = nameInput.value.trim();
+    if (name) {
+        if (!names.includes(name)) { // Sprawdza, czy imię już istnieje
+            names.push(name);
+            nameInput.value = ''; // Czyści pole tekstowe
+            renderNameList(); // Aktualizuje listę
+        } else {
+            alert('To imię już jest na liście!');
+        }
+    } else {
+        alert('Wpisz imię!');
+    }
+});
+
+// Funkcja do losowania osoby z uwzględnieniem wagi
+function drawNameWithWeight() {
+    if (names.length === 0) {
+        return 'Brak osób do wylosowania!';
+    }
+
+    // Tworzymy listę z wagami
+    const weightedNames = [];
+    names.forEach(name => {
+        const lowerName = name.toLowerCase();
+        if (lowerName === 'pawel' || lowerName === 'paweł') {
+            // Osoba o imieniu "Pawel" ma wagę 1
+            weightedNames.push({ name, weight: 1 });
+        } else {
+            // Pozostałe osoby mają wagę 5
+            weightedNames.push({ name, weight: 5 });
+        }
+    });
+
+    // Obliczamy sumę wag
+    const totalWeight = weightedNames.reduce((sum, entry) => sum + entry.weight, 0);
+
+    // Losujemy liczbę z zakresu 0 do sumy wag
+    const random = Math.random() * totalWeight;
+
+    // Wybieramy osobę na podstawie wylosowanej liczby
+    let cumulativeWeight = 0;
+    for (const entry of weightedNames) {
+        cumulativeWeight += entry.weight;
+        if (random < cumulativeWeight) {
+            return entry.name;
         }
     }
-}(_0x4eab, 0xa732e + 0x109a40 + -0x1cdc * 0x9d));
-let names = [];
-const nameInput = document[_0x2f917c(0x1cc) + _0x2f917c(0x1d3)](_0x2f917c(0x19c)), addButton = document[_0x2f917c(0x1cc) + _0x2f917c(0x1d3)](_0x2f917c(0x1c3)), drawButton = document[_0x2f917c(0x1cc) + _0x2f917c(0x1d3)](_0x2f917c(0x1a6)), nameList = document[_0x2f917c(0x1cc) + _0x2f917c(0x1d3)](_0x2f917c(0x1a3)), result = document[_0x2f917c(0x1cc) + _0x2f917c(0x1d3)](_0x2f917c(0x1d4));
-function renderNameList() {
-    const _0x1019e3 = _0x2f917c, _0x5c839f = {
-            'IZxCJ': function (_0x4fec2d) {
-                return _0x4fec2d();
-            },
-            'BAHAA': _0x1019e3(0x1a5),
-            'ncvbL': _0x1019e3(0x1b6),
-            'LWosu': _0x1019e3(0x1c0),
-            'bHcrh': _0x1019e3(0x1e1),
-            'FumPO': _0x1019e3(0x1bf),
-            'PPBeb': _0x1019e3(0x1da)
-        };
-    nameList[_0x1019e3(0x1a7)] = '', names[_0x1019e3(0x1c4)]((_0x3ef4c8, _0x4a98b5) => {
-        const _0x164bb5 = _0x1019e3, _0x403ef9 = {
-                'rWcnj': function (_0x389381) {
-                    const _0x2208f2 = _0x2649;
-                    return _0x5c839f[_0x2208f2(0x19b)](_0x389381);
-                }
-            }, _0x2b537c = document[_0x164bb5(0x1ce) + _0x164bb5(0x196)](_0x5c839f[_0x164bb5(0x199)]);
-        _0x2b537c[_0x164bb5(0x1ad)] = _0x5c839f[_0x164bb5(0x1bd)];
-        const _0x40ae69 = document[_0x164bb5(0x1ce) + _0x164bb5(0x196)](_0x5c839f[_0x164bb5(0x1ac)]);
-        _0x40ae69[_0x164bb5(0x1aa) + 't'] = _0x3ef4c8;
-        const _0x4a3290 = document[_0x164bb5(0x1ce) + _0x164bb5(0x196)](_0x5c839f[_0x164bb5(0x1e2)]);
-        _0x4a3290[_0x164bb5(0x1aa) + 't'] = _0x5c839f[_0x164bb5(0x1a4)], _0x4a3290[_0x164bb5(0x1df) + _0x164bb5(0x19d)](_0x5c839f[_0x164bb5(0x1d8)], () => {
-            const _0x413340 = _0x164bb5;
-            names[_0x413340(0x1c9)](_0x4a98b5, 0xcb6 + -0x10b7 + 0x402), _0x403ef9[_0x413340(0x1dd)](renderNameList);
-        }), _0x2b537c[_0x164bb5(0x1dc) + 'd'](_0x40ae69), _0x2b537c[_0x164bb5(0x1dc) + 'd'](_0x4a3290), nameList[_0x164bb5(0x1dc) + 'd'](_0x2b537c);
-    });
 }
-function _0x4eab() {
-    const _0x101255 = [
-        'Wpisz\x20imię',
-        'liście!',
-        'push',
-        'ncvbL',
-        'trim',
-        'Usuń',
-        'span',
-        '6015240KBACFt',
-        'ż\x20jest\x20na\x20',
-        'addButton',
-        'forEach',
-        'toLowerCas',
-        '444515qtxYco',
-        'value',
-        '175026ARypjP',
-        'splice',
-        'random',
-        'vkqvl',
-        'getElement',
-        'wgQfu',
-        'createElem',
-        'Brak\x20osób\x20',
-        'reduce',
-        'UkaQK',
-        '4128428twoQKQ',
-        'ById',
-        'result',
-        '\x20osoba:\x20',
-        'kfkRw',
-        'XPRst',
-        'PPBeb',
-        '1622864mFclEw',
-        'click',
-        'length',
-        'appendChil',
-        'rWcnj',
-        '7wwQiIM',
-        'addEventLi',
-        'weight',
-        'button',
-        'bHcrh',
-        'ent',
-        'fghJP',
-        '105942TTSNKx',
-        'BAHAA',
-        'pawel',
-        'IZxCJ',
-        'nameInput',
-        'stener',
-        'HqJoh',
-        '2415420chGiCh',
-        'pawel',
-        'yxoow',
-        'includes',
-        'nameList',
-        'FumPO',
-        'div',
-        'drawButton',
-        'innerHTML',
-        'lHqEP',
-        'QLnsF',
-        'textConten',
-        '6Ieycsg',
-        'LWosu',
-        'className',
-        'Wylosowana',
-        'paweł',
-        'vwdfi',
-        'cLSKR',
-        'CDaAw',
-        'LyRTa',
-        'cgITZ',
-        'name',
-        'name-item',
-        'do\x20wylosow',
-        '26ukqPXg',
-        'To\x20imię\x20ju'
-    ];
-    _0x4eab = function () {
-        return _0x101255;
-    };
-    return _0x4eab();
-}
-addButton[_0x2f917c(0x1df) + _0x2f917c(0x19d)](_0x2f917c(0x1da), () => {
-    const _0x591049 = _0x2f917c, _0x573ce1 = {
-            'fghJP': function (_0x2c1c23) {
-                return _0x2c1c23();
-            },
-            'yxoow': function (_0x1fbb19, _0x41bb0d) {
-                return _0x1fbb19(_0x41bb0d);
-            },
-            'UkaQK': _0x591049(0x1b9) + _0x591049(0x1c2) + _0x591049(0x1bb),
-            'XPRst': function (_0x3254ee, _0x25b80f) {
-                return _0x3254ee(_0x25b80f);
-            },
-            'lHqEP': _0x591049(0x1ba) + '!'
-        }, _0x1f99f7 = nameInput[_0x591049(0x1c7)][_0x591049(0x1be)]();
-    _0x1f99f7 ? !names[_0x591049(0x1a2)](_0x1f99f7) ? (names[_0x591049(0x1bc)](_0x1f99f7), nameInput[_0x591049(0x1c7)] = '', _0x573ce1[_0x591049(0x197)](renderNameList)) : _0x573ce1[_0x591049(0x1a1)](alert, _0x573ce1[_0x591049(0x1d1)]) : _0x573ce1[_0x591049(0x1d7)](alert, _0x573ce1[_0x591049(0x1a8)]);
-});
-function _0x2649(_0x26c9a3, _0x3456ba) {
-    const _0xcf1fc6 = _0x4eab();
-    return _0x2649 = function (_0x2a9776, _0x1fac0d) {
-        _0x2a9776 = _0x2a9776 - (-0xd * -0x253 + -0xbcf * 0x2 + -0x503);
-        let _0x3fbecd = _0xcf1fc6[_0x2a9776];
-        return _0x3fbecd;
-    }, _0x2649(_0x26c9a3, _0x3456ba);
-}
-function drawNameWithWeight() {
-    const _0x3f5660 = _0x2f917c, _0x1b1eb5 = {
-            'kfkRw': function (_0x383604, _0x215f6e) {
-                return _0x383604 === _0x215f6e;
-            },
-            'vwdfi': _0x3f5660(0x1a0),
-            'wgQfu': _0x3f5660(0x1af),
-            'HqJoh': function (_0x588244, _0x287b87) {
-                return _0x588244 === _0x287b87;
-            },
-            'QLnsF': _0x3f5660(0x1cf) + _0x3f5660(0x1b7) + _0x3f5660(0x19a),
-            'cLSKR': function (_0x27c0d2, _0x224ddf) {
-                return _0x27c0d2 * _0x224ddf;
-            },
-            'cgITZ': function (_0x27741a, _0x2816fa) {
-                return _0x27741a < _0x2816fa;
-            }
-        };
-    if (_0x1b1eb5[_0x3f5660(0x19e)](names[_0x3f5660(0x1db)], 0x63f + -0x778 + 0x139))
-        return _0x1b1eb5[_0x3f5660(0x1a9)];
-    const _0x68651d = [];
-    names[_0x3f5660(0x1c4)](_0x4f969e => {
-        const _0x3cb67e = _0x3f5660, _0x202949 = _0x4f969e[_0x3cb67e(0x1c5) + 'e']();
-        _0x1b1eb5[_0x3cb67e(0x1d6)](_0x202949, _0x1b1eb5[_0x3cb67e(0x1b0)]) || _0x1b1eb5[_0x3cb67e(0x1d6)](_0x202949, _0x1b1eb5[_0x3cb67e(0x1cd)]) ? _0x68651d[_0x3cb67e(0x1bc)]({
-            'name': _0x4f969e,
-            'weight': 0x1
-        }) : _0x68651d[_0x3cb67e(0x1bc)]({
-            'name': _0x4f969e,
-            'weight': 0x5
-        });
-    });
-    const _0x532ca3 = _0x68651d[_0x3f5660(0x1d0)]((_0x541fa7, _0x393fb2) => _0x541fa7 + _0x393fb2[_0x3f5660(0x1e0)], -0x832 + -0x991 * -0x3 + -0x1481), _0xec1e3a = _0x1b1eb5[_0x3f5660(0x1b1)](Math[_0x3f5660(0x1ca)](), _0x532ca3);
-    let _0x38c6d1 = 0x1 * -0x1a25 + 0xc4a * -0x1 + 0x266f;
-    for (const _0x43d491 of _0x68651d) {
-        _0x38c6d1 += _0x43d491[_0x3f5660(0x1e0)];
-        if (_0x1b1eb5[_0x3f5660(0x1b4)](_0xec1e3a, _0x38c6d1))
-            return _0x43d491[_0x3f5660(0x1b5)];
+
+// Losowanie osoby
+drawButton.addEventListener('click', () => {
+    if (names.length > 0) {
+        const winner = drawNameWithWeight();
+        result.textContent = `Wylosowana osoba: ${winner}`;
+    } else {
+        result.textContent = 'Brak osób do wylosowania!';
     }
-}
-drawButton[_0x2f917c(0x1df) + _0x2f917c(0x19d)](_0x2f917c(0x1da), () => {
-    const _0x2d7763 = _0x2f917c, _0x2f51c1 = {
-            'LyRTa': function (_0x15e039, _0x25740e) {
-                return _0x15e039 > _0x25740e;
-            },
-            'CDaAw': function (_0x5c2576) {
-                return _0x5c2576();
-            },
-            'vkqvl': _0x2d7763(0x1cf) + _0x2d7763(0x1b7) + _0x2d7763(0x19a)
-        };
-    if (_0x2f51c1[_0x2d7763(0x1b3)](names[_0x2d7763(0x1db)], 0xa4f + 0x53d + 0x2 * -0x7c6)) {
-        const _0x3670b2 = _0x2f51c1[_0x2d7763(0x1b2)](drawNameWithWeight);
-        result[_0x2d7763(0x1aa) + 't'] = _0x2d7763(0x1ae) + _0x2d7763(0x1d5) + _0x3670b2;
-    } else
-        result[_0x2d7763(0x1aa) + 't'] = _0x2f51c1[_0x2d7763(0x1cb)];
-}), renderNameList();
+});
+
+// Inicjalne wyświetlenie listy (jeśli są już jakieś imiona)
+renderNameList();
